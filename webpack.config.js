64c3ad550d7590dbe9c60c8e8da6d038ada6fb67
Eric Chiu <ericchiu@eric-chius-mbp.attlocal.net>
2017-05-18 03:28:22 -0700

    fix deploy

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   entry: path.join(__dirname, "src/main"),
   output: {
+    path: path.join(__dirname, "build"),
     filename: "./bundle.js"
   },
   module: {
@@ -10,17 +11,17 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['es2015']
         }
       },
       {
         test: /\.glsl$/,
-        loader: "webpack-glsl"
+        loader: "webpack-glsl-loader"
       },
       {
-        test: /\.(obj|bmp|jpg)$/,
+        test: /\.(obj|bmp)$/,
         loader: 'file-loader?name=./assets/[name]-[hash:6].[ext]'
       }
     ]
